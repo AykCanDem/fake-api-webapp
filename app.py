390b82add0cb0bf220e99410c2b158e642079ef9
AykCanDem <aykut.caner.demir@gmail.com>
2021-10-29 11:55:56 +0200

    curl requests added

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ api = Api(app)
 
 def read_data():
 
-    data = []
 
     with open("data.json", "r") as d:
-        data = json.load(d)["players"]
+        data = json.load(d)
 
     return data
 
@@ -22,8 +21,19 @@ data = read_data()
 
 @app.route("/")
 def home():
-    return render_template("/index.html", data=data)
+    return render_template("/index.html", data=data["players"])
 
+
+
+@app.route("/reset", methods = ["POST"])
+def reset():
+    if request.method == "POST":
+        with open("init_data.json", "r") as d:
+            global data 
+            data = json.load(d)
+
+    return redirect("/")
+    #return render_template("/index.html", data=data["players"])
 
 
 
@@ -31,7 +41,7 @@ class Players(Resource):
 
     def next_id(self):
         maxid = 0
-        for player in data:
+        for player in data["players"]:
             if player["player_id"] > maxid:
                 maxid = player["player_id"]
         return maxid+1
@@ -42,16 +52,23 @@ class Players(Resource):
         #if request coming from cURL request.json
         #if request coming from form submit request.form
         req = request.json if request.json != None else request.form
-        
+
         name = req["name"]
         age = req["age"]
         id = self.next_id()
 
         new_player = {"name" : name, "age" : age, "player_id" : id}
         print("POST: new_player:" , new_player)
-        data.append(new_player)
-        #print("new_player: ", new_player)
-        #print("data: ", data)
+
+        #update players list
+        data["players"].append(new_player)
+
+        #update json file
+        with open("data.json", "w") as file:
+            json.dump(data, file)
+
+
+  
         return redirect("/")
 
 class Player(Resource):
@@ -59,12 +76,47 @@ class Player(Resource):
     def get(self, player_id):
         print(data)
         print(type(data))
-        for player in data:
+        for player in data["players"]:
             if player["player_id"] == player_id:
                 return player
 
+    def put(self, player_id):
+        req = request.json
+
+        request_player = {"player_id":player_id, "name":req["name"], "age":req["age"]}
+
+        #update existing player
+        for i,player in enumerate(data["players"]):
+            if player["player_id"] == request_player["player_id"]:
+                data["players"][i] = request_player
+
+                #update json file
+                with open("data.json", "w") as file:
+                    json.dump(data, file)
+                return 200, "OK"
+        else:
+            #create new player
+            #update players list
+            data["players"].append(request_player)
+
+            #update json file
+            with open("data.json", "w") as file:
+                json.dump(data, file)
+                return 200, OK
 
 
+    def delete(self, player_id):
+
+        for i,player in enumerate(data["players"]):
+            if player["player_id"] == player_id:
+                del data["players"][i]
+
+                #update json file
+                with open("data.json", "w") as file:
+                    json.dump(data, file)
+                return "deleted"
+        else:
+            return 204, "No Content"
 
 api.add_resource(Player, "/players/<int:player_id>")
 api.add_resource(Players, "/players")
